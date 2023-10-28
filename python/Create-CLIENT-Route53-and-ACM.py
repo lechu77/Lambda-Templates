2a72630513c77e21f3483fd666a9ba2822c45e9d
Lechu <username@users.noreply.github.com>
2023-10-28 19:56:34 +0200

    adding outputs and error handling

--- python/Create-CLIENT-Route53-and-ACM.py
+++ python/Create-CLIENT-Route53-and-ACM.py
@@ -8,15 +8,20 @@ def lambda_handler(event, context):
     base_sub_domain = os.environ['BASE_SUB_DOMAIN']
     ip_address = os.environ['IP_ADDRESS']
     
+    print(f"Starting with domain: {domain_name}, subdomain: {base_sub_domain}, IP: {ip_address}")
+
     # Initialize Boto3 clients
     route53 = boto3.client('route53')
     acm = boto3.client('acm')
     acm_east = boto3.client('acm', region_name='us-east-1')
-    
+
+    print("Initialized Boto3 clients.")
+
     # Check if hosted zone exists
     hosted_zones = route53.list_hosted_zones_by_name(DNSName=domain_name)
     if hosted_zones['HostedZones']:
         hosted_zone_id = hosted_zones['HostedZones'][0]['Id'].split('/')[-1]
+        print(f"Found existing hosted zone with ID: {hosted_zone_id}")
     else:
         # Create hosted zone
         hosted_zone = route53.create_hosted_zone(
@@ -24,14 +29,17 @@ def lambda_handler(event, context):
             CallerReference=str(hash(domain_name))
         )
         hosted_zone_id = hosted_zone['HostedZone']['Id'].split('/')[-1]
-    
+        print(f"Created new hosted zone with ID: {hosted_zone_id}")
+
     # Create or Update DNS records
     dns_names = [
         f"{base_sub_domain}.{domain_name}",
         f"{base_sub_domain}-api.{domain_name}",
         f"ecoaas-api-{base_sub_domain}.{domain_name}"
     ]
-    
+
+    print(f"Creating/Updating DNS records for: {', '.join(dns_names)}")
+
     for dns_name in dns_names:
         route53.change_resource_record_sets(
             HostedZoneId=hosted_zone_id,
@@ -49,9 +57,12 @@ def lambda_handler(event, context):
                 ]
             }
         )
-    
+
+    print("DNS records successfully created/updated.")
+
     # Function to request ACM certificates
     def request_acm_certificate(acm_client, domain_names):
+        print(f"Requesting ACM certificate for: {', '.join(domain_names)}")
         response = acm_client.request_certificate(
             DomainName=domain_names[0],  # Using the first subdomain as the primary
             SubjectAlternativeNames=domain_names[1:],  # Rest as SANs
@@ -62,12 +73,16 @@ def lambda_handler(event, context):
     # Request ACM certificates
     certificate_arn = request_acm_certificate(acm, dns_names)
     certificate_arn_east = request_acm_certificate(acm_east, dns_names)
-    
+
+    print(f"ACM certificates requested. ARN: {certificate_arn}, {certificate_arn_east}")
+
     # Introduce delay to allow ACM time to populate DNS validation options
+    print("Waiting for ACM to populate DNS validation options...")
     time.sleep(10)
-    
+
     # Function to add DNS records for certificate validation
     def add_cert_validation_dns(acm_client, certificate_arn):
+        print(f"Adding DNS records for certificate validation: {certificate_arn}")
         cert_details = acm_client.describe_certificate(CertificateArn=certificate_arn)
         for validation in cert_details['Certificate']['DomainValidationOptions']:
             if 'ResourceRecord' in validation:
@@ -91,8 +106,11 @@ def lambda_handler(event, context):
     # Add DNS records for certificate validation
     add_cert_validation_dns(acm, certificate_arn)
     add_cert_validation_dns(acm_east, certificate_arn_east)
-    
+
+    print("DNS records for certificate validation added.")
+
     return {
         'statusCode': 200,
         'body': json.dumps(f'Certificates and DNS records successfully created/updated for {domain_name}.')
     }
+
